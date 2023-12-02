a9925196d84f74240f942a0441cd5e6315433e9d
rinceri <rinceri@protonmail.com>
2023-12-02 15:56:53 +0000

    use constant FILENAME

--- day 1/day01part02.py
+++ day 1/day01part02.py
@@ -1,3 +1,8 @@
+FILENAME = "day1.txt"
+
+f = open(FILENAME, "r")
+arr = f.readlines()
+f.close()
 
 def iterate_right(line: str) -> str:
     temp_str = ""
@@ -20,11 +25,6 @@ def iterate_left(line: str) -> str:
         for num in number_names.keys():
             if num in temp_str[::-1]:
                 return number_names[num]
-
-
-f = open("day 1/day1.txt", "r")
-arr = f.readlines()
-f.close()
 
 number_names = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 sum = 0
